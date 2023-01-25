e226e3f70457b6e866e313c35e1a7577239c669a
Gabe Cavazos <gabecavazos3@gmail.com>
2023-01-25 16:47:40 -0600

    using select

---
+++ select_exercises.sql
@@ -0,0 +1,32 @@
+SHOW databases;
+USE albums_db;
+DESCRIBE albums;
+SELECT * FROM albums LIMIT 2;
+SELECT DISTINCT artist FROM albums;
+SELECT release_date FROM albums 
+ORDER BY release_date DESC LIMIT 2;
+SELECT release_date FROM albums 
+ORDER BY release_date LIMIT 2;
+-- A. 6
+-- B. 23
+-- C. id
+-- D. 1967, 2011
+
+-- 4 a.
+SELECT name FROM albums 
+WHERE artist = 'Pink Floyd';
+-- b. 
+SELECT release_date FROM albums
+WHERE name = 'Sgt%';
+-- c. 
+SELECT genre FROM albums
+WHERE name = 'Nevermind';
+-- d. 
+SELECT name FROM albums
+WHERE release_date BETWEEN 1990 AND 1999;
+-- e 
+SELECT name FROM albums
+WHERE sales < 20;
+-- f Because I was very specific in using "Rock"
+SELECT name FROM albums
+WHERE genre = 'Rock';
