31a01695856ed33f2348fd5f4d9a2405d1460f3b
Gabe Cavazos <gabecavazos3@gmail.com>
2023-01-25 15:30:13 -0600

    tables

---
+++ tables_exercises.sql
@@ -0,0 +1,14 @@
+USE employees;
+SHOW TABLES;
+SHOW CREATE DATABASE employees;
+DESCRIBE employees;
+-- 5. int, date, string, bool
+-- 6. Which table(s) do you think contain a numeric type column?
+-- All but Departments
+-- 7. Which table(s) do you think contain a string type column?
+-- All but salaries
+-- 8. Which table(s) do you think contain a date type column?
+-- All but Departments
+-- 9. What is the relationship between the employees and the departments tables?
+-- dept_emp and dept_manager
+SHOW CREATE TABLE dept_manager;
